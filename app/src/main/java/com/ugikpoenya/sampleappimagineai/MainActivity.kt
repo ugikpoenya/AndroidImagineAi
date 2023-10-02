3dcfa44be38fd397d0ad693befe8a72f618db416
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2023-10-02 07:48:15 +0700

    Update sample

--- app/src/main/java/com/ugikpoenya/sampleappimagineai/MainActivity.kt
+++ app/src/main/java/com/ugikpoenya/sampleappimagineai/MainActivity.kt
@@ -3,6 +3,9 @@ package com.ugikpoenya.sampleappimagineai
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import com.ugikpoenya.imagepicker.tools.saveBitmap
 import com.ugikpoenya.imagineai.ImagineAi
@@ -18,45 +21,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+        ImagineAi(this).IMAGINE_API_KEY = ""
+        generate()
+    }
+
+    fun upscale(v: View) {
+        isLoading(true)
+        val model = ImagineAiModel()
+        model.image = saveBitmap(bitmapResult)
+        ImagineAi(this).upscale(model) { response, error -> setResult(response, error) }
+    }
+
+    fun generate(v: View) {
+        generate()
+    }
+
+    fun generate() {
+        isLoading(true)
         val prompt =
             "Cartoony, happy joe rogan portrait painting of a rabbit character from overwatch, armor, girly pink color scheme design, full shot, asymmetrical, splashscreen, organic painting, sunny day, matte painting, bold shapes, hard edges, cybernetic, moon in background, street art, trending on artstation, by huang guangjian and gil elvgren and sachin teng"
-
-        ImagineAi(this).IMAGINE_API_KEY = ""
-
         val model = ImagineAiModel()
         model.prompt = prompt
         model.style_id = 27
+        ImagineAi(this).generationsImages(model) { response, error -> setResult(response, error) }
+    }
 
-        ImagineAi(this).generationsImages(model) { response, error ->
-            if (response != null) {
-                Log.d("LOG", "Success")
-                bitmapResult = response
-                binding?.imageView?.setImageBitmap(bitmapResult)
-            }
-            Log.d("LOG", "code " + error?.code)
-            Log.d("LOG", "error " + error?.error)
-            error?.details?.forEach {
-                Log.d("LOG", "Detail $it")
-
-            }
+    private fun setResult(response: Bitmap?, error: ImagineAi.ErrorResponse?) {
+        isLoading(false)
+        if (response != null) {
+            bitmapResult = response
+            binding?.imageView?.setImageBitmap(bitmapResult)
+            Log.d("LOG", "Success " + response.width + "x" + response.height)
         }
+        Log.d("LOG", "code " + error?.code)
+        Log.d("LOG", "error " + error?.error)
+        error?.details?.forEach {
+            Log.d("LOG", "Detail $it")
 
-        binding?.btnUpscale?.setOnClickListener {
-            val upscaleModel = ImagineAiModel()
-            upscaleModel.image = saveBitmap(bitmapResult)
-            ImagineAi(this).upscale(upscaleModel) { response, error ->
-                if (response != null) {
-                    Log.d("LOG", "Success")
-                    bitmapResult = response
-                    binding?.imageView?.setImageBitmap(bitmapResult)
-                }
-                Log.d("LOG", "code " + error?.code)
-                Log.d("LOG", "error " + error?.error)
-                error?.details?.forEach {
-                    Log.d("LOG", "Detail $it")
-
-                }
-            }
         }
+    }
+
+    private fun isLoading(isLoading: Boolean) {
+        binding?.progressBar?.visibility = if (isLoading) VISIBLE else GONE
+        binding?.imageView?.visibility = if (isLoading) GONE else VISIBLE
+        binding?.btnGenerate?.visibility = binding?.imageView?.visibility!!
+        binding?.lyButton?.visibility = binding?.imageView?.visibility!!
+
     }
 }
